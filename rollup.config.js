f116bc07bd44071b907996f3e579888e78c1a8f6
imranolas <imranolas@gmail.com>
2017-12-03 16:43:33 +0000

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+
+const globals = {};
+
+export default {
+  input: "src/index.js",
+  output: {
+    file: "./dist/react-calendar.umd.js",
+    format: "umd"
+  },
+  name: "reactCalendar",
+  exports: "named",
+  globals,
+
+  sourcemap: true,
+  plugins: [
+    resolve({
+      browser: true
+    }),
+    babel({
+      exclude: "node_modules/**" // only transpile our source code
+    })
+  ]
+};
